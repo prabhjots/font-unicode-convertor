76de1f4314e0c0c335496f8feb27633d41948322
Prabhjot Singh <prabnz@gmail.com>
2019-08-31 10:37:13 +1200

    update dist

--- dist/convertor.js
+++ dist/convertor.js
@@ -26,14 +26,7 @@
             else {
                 charToAdd = stringToConvert[i];
             }
-            if (config.moveRightChars.indexOf(charToAdd) > -1) {
-                if (charToAddOnRight) {
-                    output.push(charToAddOnRight);
-                }
-                charToAddOnRight = charToAdd;
-                charToMoveRightIndex = 0;
-            }
-            else if (charToAddOnRight) {
+            if (charToAddOnRight) {
                 if (charToMoveRightIndex < 1) {
                     charToMoveRightIndex = 1;
                     output.push(charToAdd);
@@ -46,6 +39,9 @@
                     charToAddOnRight = null;
                     charToMoveRightIndex = 0;
                 }
+            }
+            else if (config.moveRightChars.indexOf(charToAdd) > -1) {
+                charToAddOnRight = charToAdd;
             }
             else if (config.moveLeftChars.indexOf(charToAdd) > -1 && output.length) {
                 insertCharOnLeft(output, config.moveAcrossCharacters, charToAdd, []);
