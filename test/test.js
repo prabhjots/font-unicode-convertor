a94a5a2b0dea5e07029e5ea592900ff487108b8f
Prabhjot Singh <prabnz@gmail.com>
2019-08-31 18:11:06 +1200

    remove lodash for now

--- test/test.js
+++ test/test.js
@@ -1,16 +1,9 @@
 const PunjabiFontConvertor = require("../dist/convertor");
-const _ = require("lodash");
-
-function toUnicode(s) {
-    return PunjabiFontConvertor.convert(s, "Arial Unicode MS", "AnmolLipi");
-}
 
 var samples = [
     { "Unicode": "ਲਿਖਿਆ", "AnmolLipi": "iliKAw", "DrChatrikWeb": "iliKaf", "Satluj": "ÇñÇÖÁÅ", "Asees": "fbfynk", "Joy": "fbfynk", "GurbaniLipi": "iliKAw", "GurmukhiLys020": "iliKAw" },
     { "Unicode": "ਵਿਦਿਆਰਥੀ", "AnmolLipi": "ividAwrQI", "DrChatrikWeb": "ividafrQI", "Satluj": "ÇòÇçÁÅðæÆ", "Asees": "ftfdnkoEh", "Joy": "ftfdnkoEh" },
     { "Unicode": ".", "AnmolLipi": ".", "DrChatrikWeb": "[", "Satluj": ".", "Asees": ".", /*"Joy": "d" */ },
-
-
 ]
 
 function getCodes(str){
@@ -59,32 +52,4 @@ test("test samples", () => {
 })
 
 
-test("basic test", () => {
-    expect(toUnicode("s")).toBe("ਸ");
-})
 
-test('convert sihary tests', () => {
-    expect(toUnicode("is")).toBe("ਸਿ");
-    expect(toUnicode("iml")).toBe("ਮਿਲ");
-    expect(toUnicode("il iK")).toBe("ਲਿ ਖਿ");
-    expect(toUnicode("iliK")).toBe("ਲਿਖਿ");
-    expect(toUnicode("iliKAw")).toBe("ਲਿਖਿਆ");
-    expect(toUnicode("ividAwrQI")).toBe("ਵਿਦਿਆਰਥੀ");
-    expect(toUnicode("isiK~Aw")).toBe("ਸਿਖਿੱਆ");
-});
-
-
-/**
- *
- *     <select id="fromFont" onchange="convertText()">
-        <option value="AnmolLipi">Anmol Lipi</option>
-        <option value="AnmolUni">Anmol Uni</option>
-        <option value="Arial Unicode MS">Unicode</option>
-        <option value="DrChatrikWeb">Dr Chatrik</option>
-        <option value="Awaze">Awaze</option>
-        <option value="Satluj">Satluj</option>
-        <option value="Asees">Asees</option>
-        <option value="Joy">Joy</option>
-        <option value="GurbaniLipi">Gurbany Lipi</option>
-        <option value="GurmukhiLys020">Gurmukhi Lys 020</option>
- */
